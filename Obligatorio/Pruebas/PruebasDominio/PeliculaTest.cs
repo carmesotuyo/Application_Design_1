b9ed4ecbbd4886deddb9b68b86767f8ba3e6abd6
Carmela <carmela.sotuyo@gmail.com>
2022-09-30 11:59:08 -0300

    RED nombre pelicula valido

---
+++ Obligatorio/Pruebas/PruebasDominio/PeliculaTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pruebas.PruebasDominio
+{
+    [TestClass]
+    public class PeliculaTest
+    {
+        [TestMethod]
+        public void NombrePeliculaValidoTest()
+        {
+            //arrange
+            Pelicula unaPelicula = new Pelicula();
+
+            //act
+            unaPelicula.Nombre = "Harry Potter";
+
+            //assert
+            Assert.AreEqual(unaPelicula.Nombre, "Harry Potter");
+        }
+    }
+}
