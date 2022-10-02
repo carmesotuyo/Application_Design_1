44c9ca835021771c1412f67b771d49bfa54494cc
Fernando Spillere <f.spillere269@gmail.com>
2022-10-02 05:13:45 -0300

    RED nombre de usuario valido

---
+++ Obligatorio/Pruebas/PruebasDominio/UsuarioTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Pruebas.PruebasDominio
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //arrange
+            Usuario unUsuario = new Usuario();
+
+            //act
+            unUsuario.Nombre = "John";
+
+            //assert
+            Assert.AreEqual(unUsuario.Nombre, "John");
+        }
+    }
+}
