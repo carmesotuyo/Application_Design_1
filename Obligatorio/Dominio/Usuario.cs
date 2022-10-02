1d6d3b6de83267a3e9ed732e6ebed9f66cfb7f01
Fernando Spillere <f.spillere269@gmail.com>
2022-10-02 14:39:51 -0300

    GREEN email y clave valida

--- Obligatorio/Dominio/Usuario.cs
+++ Obligatorio/Dominio/Usuario.cs
@@ -10,5 +10,11 @@ namespace Dominio
     {
         private string _nombreUsuario;
         public string Nombre { get => _nombreUsuario; set => _nombreUsuario = value; }
+
+        private string _email;
+        public string Email { get => _email; set => _email = value; }
+
+        private string _clave;
+        public string Clave { get => _clave; set => _clave = value; }
     }
 }
