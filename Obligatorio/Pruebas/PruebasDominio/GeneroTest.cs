b6fc2051eaac12579eb7fa11d2e512f115594191
Carmela <carmela.sotuyo@gmail.com>
2022-10-02 16:34:58 -0300

    RED nombre genero valido

---
+++ Obligatorio/Pruebas/PruebasDominio/GeneroTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+
+namespace Pruebas.PruebasDominio
+{
+    [TestClass]
+    public class GeneroTest
+    {
+        [TestMethod]
+        public void NombreGeneroValidoTest()
+        {
+            Genero unGenero = new Genero();
+            unGenero.Nombre = "Terror";
+            Assert.AreEqual(unGenero.Nombre, "Terror");
+        }
+    }
+}
