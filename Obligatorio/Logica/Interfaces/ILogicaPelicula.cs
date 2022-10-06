7fcdd7f01dbe9999c9672758f8674d73cd6a4da9
Carmela <carmela.sotuyo@gmail.com>
2022-10-06 18:37:50 -0300

    RED alta pelicula test

---
+++ Obligatorio/Logica/Interfaces/ILogicaPelicula.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dominio;
+using Repositorio;
+
+namespace Logica.Interfaces
+{
+    public interface ILogicaPelicula
+    {
+        void AltaPelicula(Pelicula pelicula, PeliculaRepo repo);
+        void BajaPelicula(Pelicula pelicula);
+        void VerInformacionDePelicula(Pelicula pelicula);
+        void MarcarComoVista(Pelicula pelicula); //va a sumar los puntos al genero para ese perfil
+
+    }
+}
