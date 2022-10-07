3b39bb7225efad370a8de1adb00f18815b238c3b
Fernando Spillere <f.spillere269@gmail.com>
2022-10-07 16:14:42 -0300

    GREEN alias perfil valido

--- Obligatorio/Dominio/Perfil.cs
+++ Obligatorio/Dominio/Perfil.cs
@@ -10,5 +10,27 @@ namespace Dominio
     {
         private string _alias;
         private int _pin;
+        private bool _esInfantil;
+        private List<GeneroPuntaje> _puntajeGeneros;
+
+        private void ValidarAlias(string value)
+        {
+
+        }
+
+        private void ValidarPin(int value)
+        {
+
+        }
+
+        public void FiltrarPelisNoAptas()
+        {
+
+        }
+        public string Alias
+        {
+            get => _alias; set => _alias = value; 
+        }
     }
+
 }
