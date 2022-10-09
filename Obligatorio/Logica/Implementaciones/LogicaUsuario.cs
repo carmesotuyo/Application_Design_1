08bd1c0ff1dc11e28dd424db395e3fd05307eac1
Fernando Spillere <f.spillere269@gmail.com>
2022-10-09 02:21:54 -0300

    REFACTOR agregar usuario test

--- Obligatorio/Logica/Implementaciones/LogicaUsuario.cs
+++ Obligatorio/Logica/Implementaciones/LogicaUsuario.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Dominio;
+using Dominio.Exceptions;
+using Logica.Interfaces;
+using Logica.Exceptions;
+using Repositorio;
 
 namespace Logica.Implementaciones
 {
-    internal class LogicaUsuario
+    public class LogicaUsuario : ILogicaUsuario
     {
+        public void RegistrarUsuario(Usuario usuario, RepoUsuarios repo)
+        {
+            ValidarDatos(usuario, repo);
+            repo.AgregarUsuario(usuario);
+        }
+
+        private static void ValidarDatos(Usuario usuario, RepoUsuarios repo)
+        {
+            ValidarNombreUnico(usuario.Nombre, repo);
+            ValidarEmailUnico(usuario.Email, repo);
+        }
+
+        private static void ValidarNombreUnico(string nombre, RepoUsuarios repo)
+        {
+            bool repetido = repo.usuarios.Where(n => n.Nombre == nombre).Any();
+            if (repetido)
+            {
+                throw new NombreUsuarioExistenteException();
+            }
+        }
+
+        private static void ValidarEmailUnico(string email, RepoUsuarios repo)
+        {
+            bool repetido = repo.usuarios.Where(n => n.Email == email).Any();
+            if (repetido)
+            {
+                throw new EmailExistenteException();
+            }
+        }
     }
 }
--- Obligatorio/Pruebas/PruebasLogica/LogicaUsuarioTest.cs
+++ Obligatorio/Pruebas/PruebasLogica/LogicaUsuarioTest.cs
@@ -8,18 +8,42 @@ using System.Threading.Tasks;
 using Repositorio;
 using Logica.Implementaciones;
 using Dominio.Exceptions;
+using Logica.Exceptions;
 
 namespace Pruebas.PruebasLogica
 {
     [TestClass]
     public class LogicaUsuarioTest
     {
-        Usuario usuario = new Usuario();
+        Usuario usuario1 = new Usuario() { Nombre = "nombreDeUsuario1", Email = "juan@da1.com", Clave = "1234567890" };
+        Usuario usuario2 = new Usuario() { Nombre = "nombreDeUsuario2", Email = "juancho@da1.com", Clave = "1234567890" };
+        Usuario usuario3 = new Usuario() { Nombre = "nombreDeUsuario3", Email = "juana@da1.com", Clave = "1234567890" };
         RepoUsuarios repo = new RepoUsuarios();
+        LogicaUsuario logica = new LogicaUsuario();
+
         [TestMethod]
         public void RegistrarUsuarioTest()
         {
-            repo.AgregarUsuario(usuario);
+            logica.RegistrarUsuario(usuario1, repo);
+            Assert.IsTrue(repo.EstaUsuario(usuario1));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NombreUsuarioExistenteException))]
+        public void RegistrarUsuarioNombreRepetidoTest()
+        {
+            usuario2.Nombre = usuario1.Nombre;
+            logica.RegistrarUsuario(usuario1, repo);
+            logica.RegistrarUsuario(usuario2, repo);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(EmailExistenteException))]
+        public void RegistrarUsuarioEmailRepetidoTest()
+        {
+            usuario2.Email = usuario1.Email;
+            logica.RegistrarUsuario(usuario1, repo);
+            logica.RegistrarUsuario(usuario2, repo);
         }
     }
 }
