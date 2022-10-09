3283b6b04e86dd3321211b3c25e885a3fc84f275
Fernando Spillere <f.spillere269@gmail.com>
2022-10-09 00:45:48 -0300

    RED agregar usuario test

---
+++ Obligatorio/Pruebas/PruebasLogica/LogicaUsuarioTest.cs
@@ -0,0 +1,25 @@
+﻿using Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repositorio;
+using Logica.Implementaciones;
+using Dominio.Exceptions;
+
+namespace Pruebas.PruebasLogica
+{
+    [TestClass]
+    public class LogicaUsuarioTest
+    {
+        Usuario usuario = new Usuario();
+        RepoUsuarios repo = new RepoUsuarios();
+        [TestMethod]
+        public void RegistrarUsuarioTest()
+        {
+            repo.AgregarUsuario(usuario);
+        }
+    }
+}
