cb77d2c139e299aca63bb96a58b393c29b0c769a
Carmela <carmela.sotuyo@gmail.com>
2022-10-08 23:49:05 -0300

    TDD puntuar pelicula con negativo test

---
+++ Obligatorio/Logica/Implementaciones/LogicaPerfil.cs
@@ -0,0 +1,43 @@
+﻿using Dominio;
+using Logica.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Implementaciones
+{
+    public class LogicaPerfil : ILogicaPerfil
+    {
+        private static int PuntajeNegativo = -1;
+        private static int PuntajePositivo = 1;
+        private static int PuntajeMuyPositivo = 2;
+
+        public List<Pelicula> FiltrarPeliculasNoAptas()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MarcarComoVista(Pelicula unaPelicula, Perfil unPerfil)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PuntuarMuyPositivo(Pelicula unaPelicula, Perfil unPerfil)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PuntuarNegativo(Pelicula unaPelicula, Perfil unPerfil)
+        {
+            unPerfil.ModificarPuntajeGenero(unaPelicula.GeneroPrincipal, PuntajeNegativo);
+
+        }
+
+        public void PuntuarPositivo(Pelicula unaPelicula, Perfil unPerfil)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Obligatorio/Pruebas/PruebasLogica/LogicaPerfilTest.cs
@@ -0,0 +1,32 @@
+﻿using Dominio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logica;
+using Logica.Implementaciones;
+
+namespace Pruebas.PruebasLogica
+{
+    [TestClass]
+    public class LogicaPerfilTest
+    {
+        LogicaPerfil logica = new LogicaPerfil();
+
+        [TestMethod]
+        public void PuntuarPeliculaNegativoTest()
+        {
+            Genero comedia = new Genero() { Nombre = "comedia" };
+            Pelicula unaPelicula = new Pelicula() { GeneroPrincipal = comedia };
+            Perfil unPerfil = new Perfil();
+            GeneroPuntaje generoPuntaje = new GeneroPuntaje() { Genero = comedia.Nombre };
+            unPerfil.AgregarGeneroPuntaje(generoPuntaje);
+
+            logica.PuntuarNegativo(unaPelicula, unPerfil);
+
+            Assert.IsTrue(unPerfil.PuntajeGeneros[0].Puntaje == -1);
+        }
+    }
+}
