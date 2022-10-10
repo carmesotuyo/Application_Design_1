9bab169d7405fdb400713ea0ebea91271d6135ac
Fernando Spillere <f.spillere269@gmail.com>
2022-10-10 05:06:55 -0300

    Creacion de pantallas de registros

--- Obligatorio/InterfazUsuario/Login.Designer.cs
+++ Obligatorio/InterfazUsuario/Login.Designer.cs
@@ -40,33 +40,21 @@
             // 
             this.lblBienvenido.AutoSize = true;
             this.lblBienvenido.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblBienvenido.Location = new System.Drawing.Point(289, 63);
-            this.lblBienvenido.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblBienvenido.Location = new System.Drawing.Point(385, 78);
             this.lblBienvenido.Name = "lblBienvenido";
-            this.lblBienvenido.Size = new System.Drawing.Size(140, 29);
+            this.lblBienvenido.Size = new System.Drawing.Size(172, 36);
             this.lblBienvenido.TabIndex = 0;
             this.lblBienvenido.Text = "Bienvenido!";
             this.lblBienvenido.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // txtCuenta
-            // 
-            this.txtCuenta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtCuenta.Location = new System.Drawing.Point(207, 130);
-            this.txtCuenta.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtCuenta.Name = "txtCuenta";
-            this.txtCuenta.Size = new System.Drawing.Size(307, 26);
-            this.txtCuenta.TabIndex = 1;
-            this.txtCuenta.Text = "Correo electrónico o username";
             // 
             // txtClave
             // 
             this.txtClave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtClave.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtClave.Location = new System.Drawing.Point(207, 181);
-            this.txtClave.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtClave.Location = new System.Drawing.Point(276, 223);
+            this.txtClave.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtClave.Name = "txtClave";
-            this.txtClave.Size = new System.Drawing.Size(307, 26);
+            this.txtClave.Size = new System.Drawing.Size(409, 30);
             this.txtClave.TabIndex = 2;
             this.txtClave.Text = "Contraseña";
             // 
@@ -74,10 +62,10 @@
             // 
             this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.Location = new System.Drawing.Point(207, 235);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnLogin.Location = new System.Drawing.Point(276, 289);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(307, 28);
+            this.btnLogin.Size = new System.Drawing.Size(409, 34);
             this.btnLogin.TabIndex = 3;
             this.btnLogin.Text = "Iniciar sesión";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -86,10 +74,9 @@
             // 
             this.lbl1.AutoSize = true;
             this.lbl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl1.Location = new System.Drawing.Point(204, 323);
-            this.lbl1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl1.Location = new System.Drawing.Point(272, 398);
             this.lbl1.Name = "lbl1";
-            this.lbl1.Size = new System.Drawing.Size(181, 17);
+            this.lbl1.Size = new System.Drawing.Size(208, 20);
             this.lbl1.TabIndex = 4;
             this.lbl1.Text = "¿No tienes una cuenta?";
             // 
@@ -97,18 +84,28 @@
             // 
             this.lblRegistrarse.AutoSize = true;
             this.lblRegistrarse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblRegistrarse.Location = new System.Drawing.Point(438, 323);
-            this.lblRegistrarse.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRegistrarse.Location = new System.Drawing.Point(584, 398);
             this.lblRegistrarse.Name = "lblRegistrarse";
-            this.lblRegistrarse.Size = new System.Drawing.Size(85, 17);
+            this.lblRegistrarse.Size = new System.Drawing.Size(101, 20);
             this.lblRegistrarse.TabIndex = 5;
             this.lblRegistrarse.TabStop = true;
             this.lblRegistrarse.Text = "Registrarme";
             this.lblRegistrarse.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
             // 
+            // txtCuenta
+            // 
+            this.txtCuenta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCuenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCuenta.Location = new System.Drawing.Point(276, 160);
+            this.txtCuenta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtCuenta.Name = "txtCuenta";
+            this.txtCuenta.Size = new System.Drawing.Size(409, 30);
+            this.txtCuenta.TabIndex = 1;
+            this.txtCuenta.Text = "Correo electrónico o username";
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.Controls.Add(this.txtCuenta);
@@ -117,9 +114,9 @@
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtClave);
             this.Controls.Add(this.lblBienvenido);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Login";
-            this.Size = new System.Drawing.Size(750, 609);
+            this.Size = new System.Drawing.Size(1000, 750);
             this.ResumeLayout(false);
             this.PerformLayout();
 
