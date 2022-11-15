d930a9d7935f9a9f88c6a341d21bf70eb9bd6ea0
Carmela <carmela.sotuyo@gmail.com>
2022-11-15 15:09:52 -0300

    RED/alta persona

---
+++ Obligatorio/Logica/Implementaciones/LogicaPersona.cs
@@ -0,0 +1,51 @@
+﻿using Dominio;
+using Logica.Exceptions;
+using Logica.Interfaces;
+using Repositorio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica.Implementaciones
+{
+    public class LogicaPersona : ILogicaPersona
+    {
+        private IPersonaRepo _repoPersona;
+
+        public LogicaPersona(IPersonaRepo repoPersona)
+        {
+            _repoPersona = repoPersona;
+        }
+
+        public List<Persona> Personas()
+        {
+            return _repoPersona.Personas();
+        }
+
+        public void AltaPersona(Persona persona, Usuario admin)
+        {
+            BloquearUsuarioNoAdmin(admin);
+            _repoPersona.AgregarPersona(persona);
+        }
+
+        public void BajaPersona(Persona persona, Usuario admin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ModificarPersona(Persona persona, Usuario admin)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BloquearUsuarioNoAdmin(Usuario admin)
+        {
+            if (!admin.EsAdministrador)
+            {
+                throw new UsuarioNoPermitidoException();
+            }
+        }
+    }
+}
---
+++ Obligatorio/Pruebas/PruebasLogica/LogicaPersonaTest.cs
@@ -0,0 +1,35 @@
+﻿using Dominio;
+using Logica.Implementaciones;
+using Logica.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Repositorio.EnDataBase;
+using Repositorio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas.PruebasLogica
+{
+    [TestClass]
+    public class LogicaPersonaTest
+    {
+        ILogicaPersona logicaPersona = new LogicaPersona(new PersonaDBRepo());
+        Persona persona = new Persona() { Id = 1, Nombre = "Juan" };
+        Usuario admin = new Usuario() { EsAdministrador = true };
+
+        [TestInitialize]
+        public void Setup()
+        {
+            DBCleanUp.CleanUp();
+        }
+
+        [TestMethod]
+        public void AgregarPersonaTest()
+        {
+            logicaPersona.AltaPersona(persona, admin);
+            Assert.IsTrue(logicaPersona.Personas().Contains(persona));
+        }
+    }
+}
