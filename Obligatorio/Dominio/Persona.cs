5501abd7b53a8debcfddb65f66c342e20a11939b
Fernando Spillere <f.spillere269@gmail.com>
2022-11-15 11:27:53 -0300

    TDD/ClasePersona

---
+++ Obligatorio/Dominio/Persona.cs
@@ -0,0 +1,37 @@
+﻿using Dominio.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Persona
+    {
+        private string _nombre;
+
+        private static void ChequearNombreValido(string value)
+        {
+            if (value == "")
+            {
+                throw new NombrePersonaVacioException();
+            }
+        }
+
+        public int Id { get; set; }
+        public string Nombre
+        {
+            get => _nombre;
+            set
+            {
+                ChequearNombreValido(value);
+                _nombre = value;
+            }
+        }
+        public string FotoPerfil { get; set; }
+        public string FechaNacimiento { get; set; }
+
+        
+    }
+}
---
+++ Obligatorio/Pruebas/PruebasDominio/PersonaTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Dominio;
+using Dominio.Exceptions;
+using Repositorio.EnDataBase;
+
+namespace Pruebas.PruebasDominio
+{
+    [TestClass]
+    public class PersonaTest
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            DBCleanUp.CleanUp();
+        }
+
+        [TestMethod]
+        public void NombrePersonaTest()
+        {
+            Persona unaPersona = new Persona()
+            {
+                Nombre = "Guillermo del Toro"
+            };
+            Assert.AreEqual(unaPersona.Nombre, "Guillermo del Toro");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NombrePersonaVacioException))]
+        public void NombrePersonaVacioTest()
+        {
+            Persona unaPersona = new Persona()
+            {
+                Nombre = ""
+            };
+        }
+
+    }
+}
