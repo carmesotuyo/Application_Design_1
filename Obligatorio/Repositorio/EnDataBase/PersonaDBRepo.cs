bf6e3d0df91f81df16a2335ceec82ba7d4bb9fbb
Fernando Spillere <f.spillere269@gmail.com>
2022-11-15 11:54:37 -0300

    Se agrega RepoPersonas

---
+++ Obligatorio/Repositorio/EnDataBase/PersonaDBRepo.cs
@@ -0,0 +1,52 @@
+﻿using Dominio;
+using Repositorio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.EnDataBase
+{
+    public class PersonaDBRepo : IPersonaRepo
+    {
+        public void AgregarPersona(Persona persona)
+        {
+            using (ThreatLevelMidnightEntertainmentDBContext tlmeContext = new ThreatLevelMidnightEntertainmentDBContext())
+            {
+                tlmeContext.Personas.Add(persona);
+                tlmeContext.SaveChanges();
+            }
+        }
+
+        public void EliminarPersona(Persona persona)
+        {
+            using (ThreatLevelMidnightEntertainmentDBContext tlmeContext = new ThreatLevelMidnightEntertainmentDBContext())
+            {
+                Persona personaABorrar = tlmeContext.Personas.FirstOrDefault(p => p.Id == persona.Id);
+                tlmeContext.Personas.Remove(personaABorrar);
+                tlmeContext.SaveChanges();
+            }
+        }
+
+        public void ModificarPersona(Persona persona)
+        {
+            using (ThreatLevelMidnightEntertainmentDBContext tlmeContext = new ThreatLevelMidnightEntertainmentDBContext())
+            {
+                Persona personaSeleccionada = tlmeContext.Personas.FirstOrDefault(p => p.Id == persona.Id);
+                personaSeleccionada = persona;
+                tlmeContext.Entry(personaSeleccionada).State = EntityState.Modified;
+                tlmeContext.SaveChanges();
+            }
+        }
+
+        public List<Persona> Personas()
+        {
+            using (ThreatLevelMidnightEntertainmentDBContext tlmeContext = new ThreatLevelMidnightEntertainmentDBContext())
+            {
+                return tlmeContext.Personas.ToList();
+            }
+        }
+    }
+}
---
+++ Obligatorio/Repositorio/Interfaces/IPersonaRepo.cs
@@ -0,0 +1,17 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositorio.Interfaces
+{
+    public interface IPersonaRepo
+    {
+        void AgregarPersona(Persona persona);
+        void EliminarPersona(Persona persona);
+        void ModificarPersona(Persona persona);
+        List<Persona> Personas();
+    }
+}
